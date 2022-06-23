d6b4fd258ec0caf9ffecb3dba7946ed044e359b7
NdzelenGlen <107994883+NdzelenGlen@users.noreply.github.com>
2022-06-23 09:37:28 -0700

    Update PROJECT.cs

--- PROJECT.cs
+++ PROJECT.cs
@@ -1,7 +1,7 @@
   using system
 namespace Calculator
 {
-    public partial class PROJECT:form;
+    public partial class Function1:form;
     {
         string input=string.Empty;
         string operand1=string.Empty;
