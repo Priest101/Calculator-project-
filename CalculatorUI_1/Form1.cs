33543977eb6260c0df01e1244110a1c762b9aff9
nkengla <duke92662@gmail.com>
2022-06-25 08:00:31 +0100

    number/basic operators ui section

---
+++ CalculatorUI_1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorUI_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bracket1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number5_Click(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bracket2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void percentage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void substraction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void equalTo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
