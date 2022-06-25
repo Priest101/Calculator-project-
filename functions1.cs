78b3bb738cccc14030aa067c3ff845ff4ca3aa57
NdzelenGlen <107994883+NdzelenGlen@users.noreply.github.com>
2022-06-25 14:31:57 +0100

    Update functions1.cs

--- functions1.cs
+++ functions1.cs
@@ -124,7 +124,6 @@ namespace Calculator
     this.operand1=string.Empty
     this.operand2=string.Empty
     }
-    
        private void buttonpercent_click(object sender,EventArgs e)  
     {
       
@@ -133,6 +132,24 @@ namespace Calculator
     operation='%';
     result=num1%;
      TextBox1.Text=result.Tostring();
+    }
+        private void buttonbackspace_Click(object sender, EventArgs e)
+    {
+       if (TextBox1.Text.Length>0)
+    {
+       TextBox1.Text = TextBox1.Text.Remove(lastCharPosi, 1);
+    }
+       if (TextBox1.Text=="")
+    {
+       TextBox1.Text = "0.0";
+    }
+       MessageBox.Show(TextBox1.Text);
+    }
+    
+      private int lastCharPosi;
+      private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+      lastCharPosi= TextBox1.Text.Length-1;
     }
        private void buttonequalsto_click(object sender,EventArgs e)  
     {
